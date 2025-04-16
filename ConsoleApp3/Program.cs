928da1b4d52ad274ed6f5c84793d83d54c1d987d
saifaldinkomi <131692348+saifaldinkomi@users.noreply.github.com>
2025-04-16 19:46:51 +0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter text: ");
+            string input=Console.ReadLine();
+            string output = "";
+            foreach (var item in input)
+            {           
+                int counter = 0;
+                    if(string.IsNullOrEmpty(output))    
+                 output += item;
+                foreach (var item1 in output)
+                {
+                    if (item1 == item)
+                        counter++;
+                }
+                if (counter == 0)
+                    output += item;
+            }
+            Console.WriteLine("after falter: "+output);
+            
+        }
+    }
+}
